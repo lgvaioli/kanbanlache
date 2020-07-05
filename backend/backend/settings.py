c115c325715b5ef61cf9267e6408070db405d5a4
Laureano G. Vaioli <laureano3400@gmail.com>
2020-07-05 15:13:53 -0300

    Added basic sign up feature.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'accounts'),
     os.path.join(BASE_DIR, 'static', 'registration'),
     os.path.join(BASE_DIR, '..', 'frontend', 'build', 'static'),
 )
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
        
 from . import views
        
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index),
 ]
