129f7977bdaa4912888ee1f2540b973d6840479a
Laureano G. Vaioli <laureano3400@gmail.com>
2020-07-11 21:36:50 -0300

    Added basic Docker stuff to deploy to localhost.
    Now we can deploy with just a "docker-compose up", which is pretty cool.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i&kihi&nrlru+gu3sd*u#myopcs-6wp8&1eu_0-&=(+p5dmbh5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost',]
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'database',
+        'PORT': 5432,
     }
 }
 
